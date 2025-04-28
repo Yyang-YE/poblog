9c5d1f8b9d6049265eb261bcb9d763a524193ce5
Yeoeun Yang <yyang78077@naver.com>
2025-04-28 09:39:21 +0900

    Feat: init project

---
+++ src/main/java/com/project/poblog/PoblogApplication.java
@@ -0,0 +1,13 @@
+package com.project.poblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/poblog/PoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
